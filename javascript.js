a921ef37e647f2f242c04c4fb2f448bb9de889f1
Connor <connorzalud@gmail.com>
2022-12-01 20:02:56 -0500

    added to lowercase feature, added game counter to track games won

--- javascript.js
+++ javascript.js
@@ -1,22 +1,14 @@
 function computerChoice(){let compChoice = (Math.floor(Math.random()*3))
 if (compChoice === 0){
-   return "Rock"}
+   return "rock"}
   else if(compChoice === 1){
-     return "Paper"
+     return "paper"
     } else if(compChoice ===2){
-      return  "Scissors"
+      return  "scissors"
     }
 }
 
-
-
-
-/*const computerSelection = computerChoice();*/
-/*const playerSelection = prompt("Choose rock, paper or scissors:");*/
-
-
-
-function playRound (playerSelection, computerSelection){
+/*function playRound (playerSelection, computerSelection){
     if (playerSelection === computerSelection){
         return "Tie!"
     } else if ((playerSelection === "Rock") && (computerSelection === "Paper") || playerSelection === "Paper" && computerSelection === "Scissors"
@@ -26,39 +18,56 @@ function playRound (playerSelection, computerSelection){
     || playerSelection === "Scissors" && computerSelection === "Paper"){
         return "You win!"
     }
-}
+}*/
 
-let playerScore = 0;
-let compScore = 0;
+        let playerScore = 0;
+        let compScore = 0;
+        let compGames = 0;
+        let playerGames = 0;
+
 function playGame(){
     for (let i = 0; i<50; i++){
         const playerSelection = prompt("Choose");
+        const playerSelectionLower = playerSelection.toLowerCase();
         const computerSelection = computerChoice();
-        console.log("You selected" + " " + playerSelection);
+        console.log("You selected" + " " + playerSelectionLower);
         console.log("Computer selected" + " " + computerSelection);
-        playRound(playerSelection,computerSelection);
-            if (playerSelection === "Rock" && computerSelection === "Paper" || playerSelection === "Paper" && computerSelection ==="Scissors"
-            || playerSelection === "Scissors" && computerSelection ==="Rock"){
+       // playRound(playerSelection,computerSelection);
+            if (playerSelectionLower === "rock" && computerSelection === "paper" || playerSelectionLower === "paper" && computerSelection ==="scissors"
+            || playerSelectionLower === "scissors" && computerSelection ==="rock"){
                     ++compScore;
                     console.log("You lose!");
-            } else if (playerSelection === "Rock" && computerSelection === "Scissors" || playerSelection === "Paper" && computerSelection === "Rock"
-            || playerSelection === "Scissors" && computerSelection === "Paper"){
+            } else if (playerSelectionLower === "rock" && computerSelection === "scissors" || playerSelectionLower === "paper" && computerSelection === "rock"
+            || playerSelectionLower === "scissors" && computerSelection === "paper"){
                 ++playerScore;
                 console.log("You win!");
-            } else if (playerSelection === computerSelection){
+            } else if (playerSelectionLower === computerSelection){
                 console.log("Tie!")
             }
         console.log("Your score is " + playerScore);
         console.log("Computer's score is " + compScore);
         if (playerScore === 5){
-            return "Game over! You win!"
+            playerScore = 0;
+            compScore = 0;
+            ++playerGames;
+            console.log("Game over! You win!");
+            checkGames();
+            return "Play again!"
         } else if (compScore === 5){
-            return "Game over! You lose!"
+            playerScore = 0;
+            compScore = 0;
+            ++compGames;
+            console.log("Game over! You lose!");
+            checkGames();
+            return "Play again!"
         }
      
     }
 }
 
-
+function checkGames(){
+    console.log("Number of games won by computer: " + compGames);
+    console.log("Number of games won by player: "+ playerGames)
+}
 
 console.log(playGame())
